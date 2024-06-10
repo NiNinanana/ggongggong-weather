5cf50fa3b3eac8ceef8a947e59ac49b597a261be
NiNinanana <yeen00123@gmail.com>
2024-06-10 17:32:47 +0900

    feat: baseurl에 실제 백엔드 주소를 가져오도록 적용

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,8 +3,9 @@ import {Platform} from 'react-native';
 
 export const api = axios.create({
   withCredentials: true,
-  baseURL:
-    Platform.OS === 'android'
+  baseURL: __DEV__
+    ? Platform.OS === 'android'
       ? 'http://10.0.2.2:4000'
-      : 'http://localhost:4000',
+      : 'http://localhost:4000'
+    : process.env.BASE_URL,
 });
