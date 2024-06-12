306424cc5fb4e4e0876ed1435875d0d8877d39cd
NiNinanana <yeen00123@gmail.com>
2024-06-12 13:05:35 +0900

    refactor: react-native-permissions 사용하여 현재 위치 가져오는 커스텀 훅 수정

--- src/hooks/useGetCurrentPosition.ts
+++ src/hooks/useGetCurrentPosition.ts
@@ -1,16 +1,60 @@
 import Geolocation from '@react-native-community/geolocation';
 import {useEffect, useState} from 'react';
 import {Coords} from '../types/geolocation';
+import {PermissionsAndroid} from 'react-native';
 
 const useGetCurrentPosition = () => {
   const [coords, setCoords] = useState<Coords>();
 
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'This app needs to access your location',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+        return true;
+      } else {
+        console.log('Location permission denied');
+        return false;
+      }
+    } catch (err) {
+      console.warn(err);
+      return false;
+    }
+  };
+
   useEffect(() => {
-    Geolocation.getCurrentPosition(info => {
-      const longitude = info.coords.longitude;
-      const latitude = info.coords.latitude;
-      setCoords(`${longitude},${latitude}`);
-    });
+    const getLocation = async () => {
+      const hasPermission = await requestLocationPermission();
+      console.log('hasPermission', hasPermission);
+      if (!hasPermission) {
+        return;
+      }
+
+      Geolocation.getCurrentPosition(
+        info => {
+          const longitude = info.coords.longitude;
+          const latitude = info.coords.latitude;
+          setCoords(`${longitude},${latitude}`);
+        },
+        error => console.log(error),
+        {
+          enableHighAccuracy: false, // 정확도를 높입니다.
+          timeout: 20000, // 20초의 시간 제한
+          maximumAge: 1000, // 캐시된 위치 정보를 최대 1초간 유지
+        },
+      );
+    };
+
+    getLocation();
   }, []);
 
   return {coords};
