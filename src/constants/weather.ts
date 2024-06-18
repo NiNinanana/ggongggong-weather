ea4ac935b92155c521937b1cf45b72949c774e73
NiNinanana <yeen00123@gmail.com>
2024-06-18 16:08:20 +0900

    feat: 초단기 예보를 가져오는 api 추가

--- src/constants/weather.ts
+++ src/constants/weather.ts
@@ -4,10 +4,30 @@ export const skyCode = {
   4: 'cloudy',
 };
 
+export const skyCodeKR = {
+  1: '맑음',
+  3: '구름 많음',
+  4: '흐림',
+};
+
 export const precipitationCode = {
   0: 'none',
   1: 'rain',
   2: 'rain/snow',
   3: 'snow',
   4: 'shower',
+  5: 'raindrops',
+  6: 'raindrops/snowflakes',
+  7: 'snowflakes',
+};
+
+export const precipitationCodeKR = {
+  0: '없음',
+  1: '비',
+  2: '비/눈',
+  3: '눈',
+  4: '소나기',
+  5: '빗방울',
+  6: '빗방울/눈날림',
+  7: '눈날림',
 };
